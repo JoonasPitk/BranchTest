c391b20c0816b8de5df4282178f51db6cc61a869
JoonasPitk <46161@edu.raseko.fi>
2022-02-10 13:09:41 +0200

    Move raise under try, and switch to mm.

--- funktiot_moduuli.py
+++ funktiot_moduuli.py
@@ -11,19 +11,23 @@ def suorakulma(sivuA, sivuB, lavistaja):
     Returns:
         float: Lävistäjän pituusvirhe, 0 -> ei virhettä
     """
-    if sivuA * sivuB * lavistaja <= 0:
-        raise ValueError('Jokaisen mitan tulee olla nollaa suurempi luku!')
     try:
+        # Tuota virhe, jos joku luvuista on nolla (Raise).
+        if sivuA * sivuB * lavistaja <= 0:
+            raise ValueError('Jokaisen mitan tulee olla nollaa suurempi luku!')
+        # Toinen tapa selvittää asia, jako nollalla -virhe.
+        # luku = 1 / (sivuA * sivuB * lavistaja)
         A_nelio = sivuA * sivuA
         B_nelio = sivuB * sivuB
         l_nelio = lavistaja * lavistaja
         pitaisi_olla = A_nelio + B_nelio
         ero = l_nelio ** 0.5 - pitaisi_olla ** 0.5
     except:
-        ero = 999
+        ero = 9999
         print('Syötetty arvo on virheellinen.')
     finally:
         return ero
+
 
 if __name__ == "__main__":
     print('Käytä ohjelmaa ajamalla sitä sovellus.py-tiedoston kautta.')
